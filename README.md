ffc75ec10c6993b2ecc469cbb180ba9d44664547
Diorgenes Morais <diorgenesmorais@gmail.com>
2023-06-27 22:39:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Interception API
+
+> Um middleware para interceptar as requests
+>
+
+- Crie um arquivo '.env' com base no modelo '.env.example
+
+```
+cat .env.example >> .env
+```
+
+- Instale as dependências
+
+```
+yarn
+```
+
+- Start na aplicação
+
+```
+yarn start
+```
---
+++ src/serve.js
@@ -0,0 +1,50 @@
+import express from "express";
+import cors from "cors";
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.disable('x-powered-by');
+app.use(express.json());
+app.use(cors());
+
+const hasBody = (body) => {
+    for (let prop in body) {
+        if (body.hasOwnProperty(prop)) return true;
+    }
+    return false;
+}
+
+const getUrl = (uri) => {
+    if (process.env.BASE_URL == 'undefined') {
+        throw Error('BASE_URL variable undefined');
+    }
+    return process.env.BASE_URL + uri;
+}
+
+app.use(async (req, res) => {
+    try {
+        const requestInit = {
+            method: req.method,
+            headers: req.headers
+        };
+        if (hasBody(req.body)) {
+            requestInit['body'] = JSON.stringify(req.body);
+        }
+        const resp = await fetch(getUrl(req.originalUrl), requestInit);
+        const text = await resp.text();
+        if (text.includes('{')) {
+            return res.status(resp.status).json(JSON.parse(text));
+        }
+        return res.status(resp.status).send(text);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+});
+
+app.get('/', (req, res, next) => {
+    res.status(200).send('Welcome the API');
+});
+
+app.listen(process.env.PORT || 4100, () => console.log('API started'));
