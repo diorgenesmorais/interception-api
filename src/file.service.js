e31487eba5a846136034a55167d1d7779ca63417
Diorgenes Morais <diorgenesmorais@gmail.com>
2023-06-29 21:41:28 -0300

    feat: get path name

--- src/file.service.js
+++ src/file.service.js
@@ -4,17 +4,17 @@ const getFiles = async (name) => {
     return JSON.parse(await fs.readFile(`./mocks/${name}.json`));
 }
 
-const handleText = (text) => {
-    return text.split('/')[1];
+const getPathname = (text) => {
+    return text.split('/')[1].split('?')[0];
 }
 
 const getFilename = (request) => {
-    return `${request.method}_${handleText(request.url)}.json`;
+    return `${request.method}_${getPathname(request.url)}`;
 }
 
 const handleRequest = async (request, response) => {
     try {
-        const mock = await getFiles(`${request.method}_${handleText(request.url)}`);
+        const mock = await getFiles(getFilename(request));
         response.status(200).json(mock);
     } catch (error) {
         response.status(400).json({Error: error.message});
@@ -24,8 +24,8 @@ const handleRequest = async (request, response) => {
 const saveFiles = async (request, response, body) => {
     try {
         fs.writeFile(
-            `./mocks/${getFilename(request)}`, 
-            JSON.stringify(body),
+            `./mocks/${getFilename(request)}.json`, 
+            JSON.stringify(body, null, 2),
             'utf-8'
         );
     } catch (error) {
